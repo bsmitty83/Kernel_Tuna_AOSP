c9e7f023b0aa446a048ef87fc9d8210250f8a2a3
Wu Fengguang <fengguang.wu@intel.com>
2011-07-25 17:12:37 -0700

    writeback: account NR_WRITTEN at IO completion time
    NR_WRITTEN is now accounted at block IO enqueue time, which is not very
    accurate as to common understanding.  This moves NR_WRITTEN accounting to
    the IO completion time and makes it more consistent with BDI_WRITTEN,
    which is used for bandwidth estimation.
    
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>
    Cc: Michael Rubin <mrubin@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -1346,7 +1346,6 @@ EXPORT_SYMBOL(account_page_dirtied);
 void account_page_writeback(struct page *page)
 {
 	inc_zone_page_state(page, NR_WRITEBACK);
-	inc_zone_page_state(page, NR_WRITTEN);
 }
 EXPORT_SYMBOL(account_page_writeback);
 
@@ -1563,8 +1562,10 @@ int test_clear_page_writeback(struct page *page)
 	} else {
 		ret = TestClearPageWriteback(page);
 	}
-	if (ret)
+	if (ret) {
 		dec_zone_page_state(page, NR_WRITEBACK);
+		inc_zone_page_state(page, NR_WRITTEN);
+	}
 	return ret;
 }
 
