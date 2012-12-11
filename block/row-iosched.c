e5385cf0640e17a49eb3e8c4402365fdcd13adc9
Tanya Brokhman <tlinder@xxxxxxxxxxxxxx>
2012-12-11 15:41:19 +0200

    block: Adding ROW scheduling algorithm
    Signed-off-by: Tatyana Brokhman <tlinder@xxxxxxxxxxxxxx>
    
    Conflicts:
    	block/Makefile
    	block/row-iosched.c

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -61,12 +61,12 @@ static const bool queue_idling_enabled[] = {
 /* Default values for row queues quantums in each dispatch cycle */
 static const int queue_quantum[] = {
 	100,	/* ROWQ_PRIO_HIGH_READ */
-	75,	/* ROWQ_PRIO_REG_READ */
-	5,	/* ROWQ_PRIO_HIGH_SWRITE */
-	4,	/* ROWQ_PRIO_REG_SWRITE */
-	4,	/* ROWQ_PRIO_REG_WRITE */
-	3,	/* ROWQ_PRIO_LOW_READ */
-	2	/* ROWQ_PRIO_LOW_SWRITE */
+	100,	/* ROWQ_PRIO_REG_READ */
+	2,	/* ROWQ_PRIO_HIGH_SWRITE */
+	1,	/* ROWQ_PRIO_REG_SWRITE */
+	1,	/* ROWQ_PRIO_REG_WRITE */
+	1,	/* ROWQ_PRIO_LOW_READ */
+	1	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues */
@@ -154,7 +154,7 @@ struct row_data {
 	unsigned int			cycle_flags;
 };
 
-#define RQ_ROWQ(rq) ((struct row_queue *) ((rq)->elevator_private[0]))
+#define RQ_ROWQ(rq) ((struct row_queue *) ((rq)->elv.priv[0]))
 
 #define row_log(q, fmt, args...)   \
 	blk_add_trace_msg(q, "%s():" fmt , __func__, ##args)
@@ -553,7 +553,7 @@ row_set_request(struct request_queue *q, struct request *rq, gfp_t gfp_mask)
 	unsigned long flags;
 
 	spin_lock_irqsave(q->queue_lock, flags);
-	rq->elevator_private[0] =
+	rq->elv.priv[0] =
 		(void *)(&rd->row_queues[get_queue_type(rq)]);
 	spin_unlock_irqrestore(q->queue_lock, flags);
 
