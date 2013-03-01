3994b5533e4ece5f97cc9abac95f58f107876235
Linus Torvalds <torvalds@linux-foundation.org>
2013-02-28 20:45:52 -0800

    lib:lzo: updated LZO libs
    http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=b0af9cd9aab60ceb17d3ebabb9fdf4ff0a99cf50
    
    Pull LZO compression update from Markus Oberhumer:
     "Summary:
      ========
    
      Update the Linux kernel LZO compression and decompression code to the
      current upstream version which features significant performance
      improvements on modern machines.
    
      Some *synthetic* benchmarks:
      ============================
    
        x86_64 (Sandy Bridge), gcc-4.6 -O3, Silesia test corpus, 256 kB block-size:
    
                         compression speed   decompression speed
    
        LZO-2005    :         150 MB/sec          468 MB/sec
        LZO-2012    :         434 MB/sec         1210 MB/sec
    
        i386 (Sandy Bridge), gcc-4.6 -O3, Silesia test corpus, 256 kB block-size:
    
                         compression speed   decompression speed
    
        LZO-2005    :         143 MB/sec          409 MB/sec
        LZO-2012    :         372 MB/sec         1121 MB/sec
    
        armv7 (Cortex-A9), Linaro gcc-4.6 -O3, Silesia test corpus, 256 kB block-size:
    
                         compression speed   decompression speed
    
        LZO-2005    :          27 MB/sec           84 MB/sec
        LZO-2012    :          44 MB/sec          117 MB/sec
      **LZO-2013-UA :          47 MB/sec          167 MB/sec
    
      Legend:
    
        LZO-2005    : LZO version in current 3.8 kernel (which is based on
                         the LZO 2.02 release from 2005)
        LZO-2012    : updated LZO version available in linux-next
      **LZO-2013-UA : updated LZO version available in linux-next plus experimental
                         ARM Unaligned Access patch. This needs approval
                         from some ARM maintainer ist NOT YET INCLUDED."
    
    Andrew Morton <akpm@linux-foundation.org> acks it and says:
     "There's a new LZ4 on the block which is even faster than the sped-up
      LZO, but various filesystems and things use LZO"
    
    * tag 'lzo-update-signature-20130226' of git://github.com/markus-oberhumer/linux:
      crypto: testmgr - update LZO compression test vectors
      lib/lzo: Update LZO compression to current upstream version
      lib/lzo: Rename lzo1x_decompress.c to lzo1x_decompress_safe.c
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>
    
    Conflicts:
    	lib/lzo/lzo1x_compress.c
    	lib/lzo/lzo1x_decompress.c
    	lib/lzo/lzodefs.h
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- lib/lzo/lzodefs.h
+++ lib/lzo/lzodefs.h
@@ -29,7 +29,7 @@
 #define LZO_USE_CTZ32	1
 #elif defined(__i386__) || defined(__powerpc__)
 #define LZO_USE_CTZ32	1
-#else
+#elif defined(__arm__) && (__LINUX_ARM_ARCH__ >= 5)
 #define LZO_USE_CTZ32	1
 #endif
 
@@ -57,7 +57,3 @@
 #define D_SIZE		(1u << D_BITS)
 #define D_MASK		(D_SIZE - 1)
 #define D_HIGH		((D_MASK >> 1) + 1)
-
-#define DX2(p, s1, s2)	(((((size_t)((p)[2]) << (s2)) ^ (p)[1]) \
-							<< (s1)) ^ (p)[0])
-#define DX3(p, s1, s2, s3)	((DX2((p)+1, s2, s3) << (s1)) ^ (p)[0])
