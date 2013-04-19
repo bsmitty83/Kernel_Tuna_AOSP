d7fb7915d29220302e78ac8aab523db6d60418a9
Francisco Franco <franciscofranco.1990@gmail.com>
2013-04-19 14:01:22 -0700

    block: row: this should be 2.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    Conflicts:
    	block/row-iosched.c

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -61,12 +61,12 @@ static const bool queue_idling_enabled[] = {
 /* Default values for row queues quantums in each dispatch cycle */
 static const int queue_quantum[] = {
 	100,	/* ROWQ_PRIO_HIGH_READ */
-	100,	/* ROWQ_PRIO_REG_READ */
-	2,	/* ROWQ_PRIO_HIGH_SWRITE */
-	1,	/* ROWQ_PRIO_REG_SWRITE */
-	1,	/* ROWQ_PRIO_REG_WRITE */
-	1,	/* ROWQ_PRIO_LOW_READ */
-	1	/* ROWQ_PRIO_LOW_SWRITE */
+	75,	/* ROWQ_PRIO_REG_READ */
+	5,	/* ROWQ_PRIO_HIGH_SWRITE */
+	4,	/* ROWQ_PRIO_REG_SWRITE */
+	4,	/* ROWQ_PRIO_REG_WRITE */
+	3,	/* ROWQ_PRIO_LOW_READ */
+	2	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues */
