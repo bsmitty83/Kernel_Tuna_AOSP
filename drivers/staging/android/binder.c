aabbd8adf36e161a60fcfde001a9c8c00eab8622
Bojan Prtvar <prtvar.b@gmail.com>
2013-09-01 20:30:38 +0200

    Staging: android: add __user annotation in binder.c
    This fixes the following sparse error
    drivers/staging/android/binder.c:1795:36: error: incompatible types in comparison expression (different address spaces)
    
    Change-Id: I3824cb700d0de0e24c94771b1441e639d7d4d18b
    Signed-off-by: Bojan Prtvar <prtvar.b@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -1881,7 +1881,7 @@ static int binder_thread_write(struct binder_proc *proc, struct binder_thread *t
 		case BC_INCREFS_DONE:
 		case BC_ACQUIRE_DONE: {
 			void __user *node_ptr;
-			void *cookie;
+			void __user *cookie;
 			struct binder_node *node;
 
 			if (get_user(node_ptr, (void * __user *)ptr))
