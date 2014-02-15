9edc70afed9255cbc7b0cd1d841507fa5a5e34e2
bsmitty <bsmitty83@yahoo.com>
2014-02-14 22:29:30 -0500

    Revert "slub: missing test for partial pages flush work in flush_all()"
    This reverts commit bf69ff2afc0a8100be7b2bb35260ea92a68a5fac.

--- mm/slub.c
+++ mm/slub.c
@@ -2025,7 +2025,7 @@ static bool has_cpu_slab(int cpu, void *info)
 	struct kmem_cache *s = info;
 	struct kmem_cache_cpu *c = per_cpu_ptr(s->cpu_slab, cpu);
 
-	return c->page || c->partial;
+	return !!(c->page);
 }
 
 static void flush_all(struct kmem_cache *s)
