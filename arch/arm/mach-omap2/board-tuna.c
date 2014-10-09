57da1f0809171496d953b2a77f30081ef2d2ad94
Kyle Repinski <repinski23@gmail.com>
2014-10-09 16:37:46 -0500

    tuna: update to latest kernel changes.

--- arch/arm/mach-omap2/board-tuna.c
+++ arch/arm/mach-omap2/board-tuna.c
@@ -1361,6 +1361,13 @@ static void __init tuna_reserve(void)
 {
 	omap_init_ram_size();
 
+#ifdef CONFIG_ION_OMAP
+	tuna_android_display_setup(get_omap_ion_platform_data());
+	omap_ion_init();
+#else
+	tuna_android_display_setup(NULL);
+#endif
+
 	omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
 						OMAP_RAM_CONSOLE_SIZE_DEFAULT);
 
@@ -1370,8 +1377,9 @@ static void __init tuna_reserve(void)
 	memblock_remove(PHYS_ADDR_DUCATI_MEM, PHYS_ADDR_DUCATI_SIZE);
 
 	/* ipu needs to recognize secure input buffer area as well */
-	omap_ipu_set_static_mempool(PHYS_ADDR_DUCATI_MEM,
-								PHYS_ADDR_DUCATI_SIZE + OMAP4_ION_HEAP_SECURE_INPUT_SIZE);
+	omap_ipu_set_static_mempool(PHYS_ADDR_DUCATI_MEM, PHYS_ADDR_DUCATI_SIZE +
+					OMAP4_ION_HEAP_SECURE_INPUT_SIZE +
+					OMAP4_ION_HEAP_SECURE_OUTPUT_WFDHDCP_SIZE);
 
 #ifdef CONFIG_OMAP_REMOTE_PROC_DSP
 	memblock_remove(PHYS_ADDR_TESLA_MEM, PHYS_ADDR_TESLA_SIZE);
@@ -1379,12 +1387,6 @@ static void __init tuna_reserve(void)
 								PHYS_ADDR_TESLA_SIZE);
 #endif
 
-#ifdef CONFIG_ION_OMAP
-	tuna_android_display_setup(get_omap_ion_platform_data());
-	omap_ion_init();
-#else
-	tuna_android_display_setup(NULL);
-#endif
 	omap_reserve();
 }
 
