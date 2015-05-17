bcdf8b516a95bb5d737ca87a4f378ae766d49231
Brian Smith <b671983@gmail.com>
2015-05-17 19:57:19 -0400

    Zeneractive cpu governor modified for dual-core cpus

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,7 +389,10 @@ extern struct cpufreq_governor cpufreq_gov_sakuractive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sakuractive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
 extern struct cpufreq_governor cpufreq_gov_adaptive;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_adaptive)   
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_adaptive) 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZENERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_zeneractive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zeneractive)  
 #endif
 
 
