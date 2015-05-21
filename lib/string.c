ddb87fd06372979d876f48f6ed53a244b6e349dc
pattyboi <patpat1325179@gmail.com>
2015-05-20 20:57:34 -0400

    ARM:lib/string.c: so,the standard string functions arent that wonderful on arm,we can gain much better performance by using some optimized ones.. so,lets import linaros cortex-string memchr and strcpy,grab GABRIEL GONZALEZ's strchr & strlen all code links: inital idea of porting linaros stuff: https://github.com/kosi2801/LGP990_KosiKernel/commit/06d6a0368eb0e09be1a33057386a07a6d6e1ced5 gabriel's website posts: http://www.gabrielgonzalezgarcia.com/2012/10/02/strchr-optimized-for-arm/ http://www.gabrielgonzalezgarcia.com/2012/09/28/arm-optimized-strlen/ linaros cortex string git/gerrit site https://git.linaro.org/toolchain/cortex-strings.git/tree/HEAD:/src/arm
    Conflicts:
    	Makefile
    	lib/Makefile
    	lib/string.c

--- lib/string.c
+++ lib/string.c
@@ -24,6 +24,7 @@
 #include <linux/ctype.h>
 #include <linux/module.h>
 #include <linux/memcopy.h>
+#include <linux/linkage.h>
 
 #ifndef __HAVE_ARCH_STRNICMP
 /**
@@ -92,13 +93,143 @@ EXPORT_SYMBOL(strncasecmp);
  * @src: Where to copy the string from
  */
 #undef strcpy
-char *strcpy(char *dest, const char *src)
-{
-	char *tmp = dest;
 
-	while ((*dest++ = *src++) != '\0')
-		/* nothing */;
-	return tmp;
+#ifdef __thumb2__
+#define magic1(REG) "#0x01010101"
+#define magic2(REG) "#0x80808080"
+#else
+#define magic1(REG) #REG
+#define magic2(REG) #REG ", lsl #7"
+#endif
+
+char* __attribute__((naked))
+strcpy (char* dst, const char* src)
+{
+  asm (
+#if !(defined(__OPTIMIZE_SIZE__) || defined (PREFER_SIZE_OVER_SPEED) || \
+      (defined (__thumb__) && !defined (__thumb2__)))
+       "pld	[r1, #0]\n\t"
+       "eor	r2, r0, r1\n\t"
+       "mov	ip, r0\n\t"
+       "tst	r2, #3\n\t"
+       "bne	4f\n\t"
+       "tst	r1, #3\n\t"
+       "bne	3f\n"
+  "5:\n\t"
+#ifndef __thumb2__
+       "str	r5, [sp, #-4]!\n\t"
+       "mov	r5, #0x01\n\t"
+       "orr	r5, r5, r5, lsl #8\n\t"
+       "orr	r5, r5, r5, lsl #16\n\t"
+#endif
+
+       "str	r4, [sp, #-4]!\n\t"
+       "tst	r1, #4\n\t"
+       "ldr	r3, [r1], #4\n\t"
+       "beq	2f\n\t"
+       "sub	r2, r3, "magic1(r5)"\n\t"
+       "bics	r2, r2, r3\n\t"
+       "tst	r2, "magic2(r5)"\n\t"
+       "itt	eq\n\t"
+       "streq	r3, [ip], #4\n\t"
+       "ldreq	r3, [r1], #4\n"
+       "bne	1f\n\t"
+       /* Inner loop.  We now know that r1 is 64-bit aligned, so we
+	  can safely fetch up to two words.  This allows us to avoid
+	  load stalls.  */
+       ".p2align 2\n"
+  "2:\n\t"
+       "pld	[r1, #8]\n\t"
+       "ldr	r4, [r1], #4\n\t"
+       "sub	r2, r3, "magic1(r5)"\n\t"
+       "bics	r2, r2, r3\n\t"
+       "tst	r2, "magic2(r5)"\n\t"
+       "sub	r2, r4, "magic1(r5)"\n\t"
+       "bne	1f\n\t"
+       "str	r3, [ip], #4\n\t"
+       "bics	r2, r2, r4\n\t"
+       "tst	r2, "magic2(r5)"\n\t"
+       "itt	eq\n\t"
+       "ldreq	r3, [r1], #4\n\t"
+       "streq	r4, [ip], #4\n\t"
+       "beq	2b\n\t"
+       "mov	r3, r4\n"
+  "1:\n\t"
+#ifdef __ARMEB__
+       "rors	r3, r3, #24\n\t"
+#endif
+       "strb	r3, [ip], #1\n\t"
+       "tst	r3, #0xff\n\t"
+#ifdef __ARMEL__
+       "ror	r3, r3, #8\n\t"
+#endif
+       "bne	1b\n\t"
+       "ldr	r4, [sp], #4\n\t"
+#ifndef __thumb2__
+       "ldr	r5, [sp], #4\n\t"
+#endif
+       "BX LR\n"
+
+       /* Strings have the same offset from word alignment, but it's
+	  not zero.  */
+  "3:\n\t"
+       "tst	r1, #1\n\t"
+       "beq	1f\n\t"
+       "ldrb	r2, [r1], #1\n\t"
+       "strb	r2, [ip], #1\n\t"
+       "cmp	r2, #0\n\t"
+       "it	eq\n"
+       "BXEQ LR\n"
+  "1:\n\t"
+       "tst	r1, #2\n\t"
+       "beq	5b\n\t"
+       "ldrh	r2, [r1], #2\n\t"
+#ifdef __ARMEB__
+       "tst	r2, #0xff00\n\t"
+       "iteet	ne\n\t"
+       "strneh	r2, [ip], #2\n\t"
+       "lsreq	r2, r2, #8\n\t"
+       "streqb	r2, [ip]\n\t"
+       "tstne	r2, #0xff\n\t"
+#else
+       "tst	r2, #0xff\n\t"
+       "itet	ne\n\t"
+       "strneh	r2, [ip], #2\n\t"
+       "streqb	r2, [ip]\n\t"
+       "tstne	r2, #0xff00\n\t"
+#endif
+       "bne	5b\n\t"
+       "BX LR\n"
+
+       /* src and dst do not have a common word-alignement.  Fall back to
+	  byte copying.  */
+  "4:\n\t"
+       "ldrb	r2, [r1], #1\n\t"
+       "strb	r2, [ip], #1\n\t"
+       "cmp	r2, #0\n\t"
+       "bne	4b\n\t"
+       "BX LR"
+
+#elif !defined (__thumb__) || defined (__thumb2__)
+       "mov	r3, r0\n\t"
+  "1:\n\t"
+       "ldrb	r2, [r1], #1\n\t"
+       "strb	r2, [r3], #1\n\t"
+       "cmp	r2, #0\n\t"
+       "bne	1b\n\t"
+       "BX LR"
+#else
+       "mov	r3, r0\n\t"
+  "1:\n\t"
+       "ldrb	r2, [r1]\n\t"
+       "add	r1, r1, #1\n\t"
+       "strb	r2, [r3]\n\t"
+       "add	r3, r3, #1\n\t"
+       "cmp	r2, #0\n\t"
+       "bne	1b\n\t"
+       "BX LR"
+#endif
+       );
 }
 EXPORT_SYMBOL(strcpy);
 #endif
@@ -282,8 +413,8 @@ int strncmp(const char *cs, const char *ct, size_t count)
 }
 EXPORT_SYMBOL(strncmp);
 #endif
-
 #ifndef __HAVE_ARCH_STRCHR
+#if 0
 /**
  * strchr - Find the first occurrence of a character in a string
  * @s: The string to be searched
@@ -296,6 +427,7 @@ char *strchr(const char *s, int c)
 			return NULL;
 	return (char *)s;
 }
+#endif
 EXPORT_SYMBOL(strchr);
 #endif
 
@@ -376,6 +508,7 @@ char *strim(char *s)
 EXPORT_SYMBOL(strim);
 
 #ifndef __HAVE_ARCH_STRLEN
+#if 0
 /**
  * strlen - Find the length of a string
  * @s: The string to be sized
@@ -388,6 +521,7 @@ size_t strlen(const char *s)
 		/* nothing */;
 	return sc - s;
 }
+#endif
 EXPORT_SYMBOL(strlen);
 #endif
 
@@ -731,6 +865,7 @@ EXPORT_SYMBOL(strnstr);
 #endif
 
 #ifndef __HAVE_ARCH_MEMCHR
+#if 0
 /**
  * memchr - Find a character in an area of memory.
  * @s: The memory area
@@ -750,5 +885,6 @@ void *memchr(const void *s, int c, size_t n)
 	}
 	return NULL;
 }
+#endif
 EXPORT_SYMBOL(memchr);
 #endif
